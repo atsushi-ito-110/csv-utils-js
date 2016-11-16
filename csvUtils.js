788dfdbe67899249e60e15b1fbc29b8a11ac06e2
a-ito <a-ito@iret.co.jp>
2016-11-16 17:36:19 +0900

    The first commit

---
+++ csvUtils.js
@@ -0,0 +1,47 @@
+var CsvUtils = function () {
+};
+
+// toHashMap
+CsvUtils.prototype.toHashMap = function (header_line, lines) {
+  try {
+    var createEntries = function (headers, columns) {
+      var entries = {};
+      for (var i = 0; i < columns.length; i++) {
+        var key = headers[i];
+        var value = columns[i];
+        if (key.split(".").length <= 1) {
+          entries[key] = value;
+          continue;
+        }
+
+        var split_keys = key.split(".");
+        var target = entries;
+        key = split_keys.pop();
+        for (var j = 0; j < split_keys.length; j++) {
+          var split_key = split_keys[j];
+          if (!target[split_key]) {
+            target[split_key] = {};
+          }
+          target = target[split_key];
+        }
+        target[key] = value;
+      }
+      return entries;
+    };
+
+    var headers = header_line.split(",");
+    console.log("headers: ", JSON.stringify(headers, null, 2));
+    var entries_list = [];
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i];
+      var columns = line.split(",");
+      console.log("columns: ", JSON.stringify(columns, null, 2));
+      var entries = createEntries(headers, columns);
+      entries_list.push(entries);
+    }
+
+    return entries_list;
+  } catch (e) {
+    throw e;
+  }
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+var header_line = "id,name,profile.birth.year,profile.birth.day,profile.blood,profile.address";
+var lines = [
+  "1,ito,1990,06-06,O,Minato-Ku Tokyo",
+  "2,ito,1991,06-06,O,Minato-Ku Tokyo"
+];
+var entries_list = CsvUtils.prototype.toHashMap(header_line, lines);
+console.log("entries_list: ", JSON.stringify(entries_list, null, 2));
